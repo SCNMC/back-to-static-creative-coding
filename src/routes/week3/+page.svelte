77dcc60a648dc242d90197f19cf4cdaf50c54eb1
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-12-22 22:09:21 +0100

    finished page transition v1

--- src/routes/week3/+page.svelte
+++ src/routes/week3/+page.svelte
@@ -1,10 +1,60 @@
 <script>
-    import { onMount } from "svelte";
+	import { onMount } from "svelte";
 	import { gsap } from "gsap/dist/gsap";
-    onMount(() => {
-	
+  import { scale } from "svelte/transition";
+	onMount(() => {
+		var textWrapper = document.querySelector("h2");
+		textWrapper.innerHTML = textWrapper.textContent.replace(
+			/\S/g,
+			"<span class='letter'>$&</span>"
+		);
 
-		
+		gsap.timeline().add({
+			targets: ".title .letter  h2",
+			translateY: [100, 0],
+			translateZ: 0,
+			opacity: [0, 1],
+			easing: "easeOutExpo",
+			duration: 2000,
+			delay: (el, i) => 4800 + 40 * i,
+		});
+
+		gsap.to(".background", 2.4, {
+			y: "-100%",
+			ease: "Expo.easeInOut",
+			delay: 1,
+		});
+
+		gsap.to("img", 4, {
+			scale: "1",
+			ease: "Expo.easeInOut",
+			delay: 0,
+		});
+
+		gsap.to("img", 2.4, {
+			width: "500",
+			height: "700",
+			ease: "Expo.easeInOut",
+			delay: 3.4,
+		});
+
+		gsap.from("img", 0.4, {
+			opacity: 0,
+			ease: "Expo.easeInOut",
+			delay: 3.4,
+		});
+
+		gsap.from(
+			"header , .extra-info",
+			2,
+			{
+				opacity: 0,
+				y: 30,
+				ease: "Expo.easeInOut",
+				delay: 4.2,
+			},
+			0.1
+		);
 	});
 </script>
 
@@ -22,29 +72,34 @@
 	</header>
 
 	<section>
-		<h2>creative
-        </h2>
+		<div class="background" />
 		<img src="/assets/img1.jpg" alt="image" />
+        <h2>creative </h2>
 	</section>
-   <section class="extra-info">
-    <p>much creative</p>
-    <p>very creative</p>
-   </section>
+	<section class="extra-info">
+		<p>much creative</p>
+		<p>very creative</p>
+	</section>
 </body>
 
 <style>
+
+	.title .letter {
+		display: inline-block;
+		line-height: 1em;
+	}
 	@font-face {
 		font-family: "Monument Extended";
 		src: url("/assets/Fonts/MonumentExtended-Regular.otf");
 	}
-    @font-face {
+	@font-face {
 		font-family: "Voyage";
 		src: url("/assets/Fonts/Voyage.ttf");
 	}
-    @font-face {
-    font-family: 'NewYork';
-    src:url('/assets/Fonts/NewYork.otf');
-}
+	@font-face {
+		font-family: "NewYork";
+		src: url("/assets/Fonts/NewYork.otf");
+	}
 
 	header {
 		display: flex;
@@ -53,40 +108,53 @@
 		justify-content: space-between;
 		gap: 1em;
 		padding: 2em 2em;
-        font-family: "Monument Extended";
+		font-family: "Monument Extended";
 	}
+    h1{
+        font-family: "Monument Extended";
+    }
 	ul {
 		display: flex;
 		flex-direction: row;
 		gap: 1em;
 	}
-    body{
-        background: #bebeb8;
-        height: 100vh;
-    }
+	body {
+		background: #bebeb8;
+		height: 100vh;
+	}
 	* {
 		color: #000;
 	}
 	p {
 		font-size: 1em;
-        font-family: "Monument Extended";
+		font-family: "Monument Extended";
 	}
-    h2 {
+	h2 {
 		position: absolute;
 		background: none;
-        font-size: 8em;
-        font-family: "NewYork";
-        font-weight: 200;
-        color: 	#2584ec;
+		font-size: 8em;
+		font-family: "NewYork";
+		font-weight: 200;
+		color: #2584ec;
 	}
 	h1 {
 		font-size: 1em;
+	}
+	.background {
+		background: #bebeb8;
+		opacity: 1;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		z-index: 2;
 	}
 	section {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-        flex-direction: column;
+		flex-direction: column;
 	}
 
 	img {
@@ -95,13 +163,13 @@
 		height: 60vh;
 		object-fit: cover;
 	}
-    .extra-info{
-            display: flex;
-            flex-direction: row;
-         justify-content: space-between;
-         align-items: stretch;
-         padding: 2em 5em;
-        }
+	.extra-info {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: stretch;
+		padding: 2em 5em;
+	}
 
 	@media (min-width: 70rem) {
 		header {
@@ -122,31 +190,30 @@
 		h1 {
 			font-size: 1.5em;
 		}
-        h2 {
-		position: absolute;
-		background: none;
-        font-size: 17em;
-        font-family: "NewYork";
-        font-weight: 200;
-        color: 	#2584ec;
-	}
+		h2 {
+			position: absolute;
+			background: none;
+			font-size: 17em;
+			font-family: "NewYork";
+			font-weight: 200;
+			color: #2584ec;
+		}
 		section {
 			display: flex;
 			align-items: center;
 			justify-content: center;
 		}
 		img {
-			padding: 3em 4em;
-			width: 30%;
-			height: 65vh;
-			object-fit: cover;
+			width: 70%;
+			height: 60vh;
 		}
-        .extra-info{
-            display: flex;
-            flex-direction: row;
-         justify-content: space-between;
-         align-items: stretch;
-         padding: 2em 5em;
-        }
+		.extra-info {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: stretch;
+			padding: 2em 5em;
+            margin-top: -3em;
+		}
 	}
 </style>
